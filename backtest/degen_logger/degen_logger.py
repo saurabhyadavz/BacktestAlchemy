8d84185bd2fde16c463d7103115de9f83bf8bb8d
Saurabh Yadav <saurabhshadow30@gmail.com>
2023-04-25 17:36:50 +0530

    add degen logger package

---
+++ backtest/degen_logger/degen_logger.py
@@ -0,0 +1,24 @@
+import logging
+
+
+class DegenLogger:
+    def __init__(self, log_file):
+        self.logger = logging.getLogger(__name__)
+        self.logger.setLevel(logging.DEBUG)
+
+        # create file handler which logs messages to a file
+        fh = logging.FileHandler(log_file)
+        fh.setLevel(logging.DEBUG)
+
+        # create console handler with a higher log level
+        ch = logging.StreamHandler()
+        ch.setLevel(logging.INFO)
+
+        # create formatter and add it to the handlers
+        formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+        fh.setFormatter(formatter)
+        ch.setFormatter(formatter)
+
+        # add the handlers to the logger
+        self.logger.addHandler(fh)
+        self.logger.addHandler(ch)
