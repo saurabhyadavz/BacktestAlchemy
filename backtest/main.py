1ce357115a34b6fd8deb9d1a336fe281ef677890
Saurabh Yadav <saurabhshadow30@gmail.com>
2023-04-25 11:00:17 +0530

    fix strategy paramaters

--- backtest/main.py
+++ backtest/main.py
@@ -7,10 +7,13 @@ from backtest.data.data import DataProducer
 from backtest.strategy.strategy import Strategy
 from backtest.opt_backtest.opt_backtest import OptBacktest
 from backtest.analyzers.analyzers import Analyzers
+
 if __name__ == "__main__":
-    data = DataProducer("BANKNIFTY", "2020-01-01", "2020-02-20", "1min")
-    strategy = Strategy(instrument="BANKNIFTY", is_intraday=False, start_time="9:25", end_time="15:00",
+    strategy = Strategy(start_date="2020-01-01", end_date="2020-02-20",
+                        instrument="BANKNIFTY", is_intraday=True,
+                        start_time="9:25", end_time="15:00",
                         stop_loss=0.2, move_sl_to_cost=True)
+    data = DataProducer(strategy.instrument, strategy.start_date, strategy.end_date, strategy.timeframe)
     bt = OptBacktest(strategy, data)
     points = bt.backtest_simple_straddle()
     analyzer = Analyzers()
--- backtest/strategy/strategy.py
+++ backtest/strategy/strategy.py
@@ -2,11 +2,13 @@ from typing import Union
 from backtest.utils import utils
 
 class Strategy:
-    def __init__(self, instrument: str, is_intraday: bool, start_time: str, end_time: str,
+    def __init__(self, start_date: str, end_date: str, instrument: str, is_intraday: bool, start_time: str, end_time: str,
                  stop_loss: Union[int, float] = None, target: Union[int, float] = None,
                  per_trade_commission: float = 0, slippage: float = 0,
                  re_entry_count: int = 0, re_execute_count: int = 0,
                  timeframe: str = "1min", move_sl_to_cost: bool = True):
+        self.start_date = start_date
+        self.end_date = end_date
         self.instrument = instrument
         self.is_intraday = is_intraday
         self.start_time = utils.str_to_time_obj(start_time)
